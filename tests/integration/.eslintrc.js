3a8a30d3502ebd2942ab5156fac35564222f9d77
nicmosc <nicmosc@users.noreply.github.com>
2020-04-22 17:48:02 +0200

    dont use relative path for tsconfig

--- tests/integration/.eslintrc.js
+++ tests/integration/.eslintrc.js
@@ -2,8 +2,8 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@drawbotics/eslint-plugin-deprecated-props'],
   parserOptions: {
-    'sourceType': 'module',
-    project: './tsconfig.json'
+    sourceType: 'module',
+    project: 'tsconfig.json',
   },
   rules: {
     '@typescript-eslint/no-unused-vars': ['off'],
